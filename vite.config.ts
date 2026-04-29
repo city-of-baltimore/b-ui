b09df9c31b38368593c49530751e404f4c0ffad1
Xander Jake de los Santos <xanderjake.delossantos@baltimorecity.gov>
2026-04-29 16:17:02 -0400

    add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,59 @@
-import { defineConfig } from 'vite'
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), dts({
-      tsconfigPath: resolve(__dirname, "tsconfig.lib.json")
+    tsconfigPath: resolve(__dirname, "tsconfig.lib.json")
   })],
   build: {
-      copyPublicDir: false,
-      lib: {
-          entry: resolve(__dirname, 'lib/main.ts'),
-          formats: ['es']
-      },
-      rollupOptions: {
-          external: ['react', 'react/jsx-runtime'],
-          input: Object.fromEntries(
-              glob.sync('lib/**/*.{ts,tsx}', {
-                  ignore: ["lib/**/*.d.ts"],
-              }).map(file => [
-                  relative(
-                      'lib',
-                      file.slice(0, file.length - extname(file).length)
-                  ),
-                  fileURLToPath(new URL(file, import.meta.url))
-              ])
-          ),
-          output: {
-              assetFileNames: 'assets/[name][extname]',
-              entryFileNames: '[name].js'
-          }
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(glob.sync('lib/**/*.{ts,tsx}', {
+        ignore: ["lib/**/*.d.ts"]
+      }).map(file => [relative('lib', file.slice(0, file.length - extname(file).length)), fileURLToPath(new URL(file, import.meta.url))])),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
       }
+    }
+  },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
   }
-  
-})
+});
